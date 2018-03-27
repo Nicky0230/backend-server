8090e85792431912142fddb180415d07b71ae0bc
Nicky0230 <Bernarda3008@gmail.com>
2018-03-27 17:06:54 -0400

    Seccion 10 semi-avanzada

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializa variables
 var app = express();
+
+
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
 
 // Conecion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res) => {
@@ -15,14 +27,8 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res) 
 })
 
 // Rutas
-app.get( '/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
